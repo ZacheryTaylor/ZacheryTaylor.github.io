e304b5a4854510a3166be90adc0bd18ae0e63822
ZacheryTaylor <80860181+ZacheryTaylor@users.noreply.github.com>
2026-08-05 14:33:57 -0500

    Update personal-projects-data.js

--- assets/js/personal-projects-data.js
+++ assets/js/personal-projects-data.js
@@ -8,7 +8,7 @@ const projects = [
     cardDescription:
       "A personal quote archive tool that exports the latest Rich Dad Poor Dad quote bank to a structured PDF report.",
   
-    coverImage: "images/personal/quotes/QuoteBank.png", // adjust path or image name
+    coverImage: "images/personal/quotes/Quotes.png",
     coverAlt: "Quote bank flashcard and log interface",
   
     description:
